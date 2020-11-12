0aceeb08ac96d0efba7968b6099ed733390eb7c9
matias <matiasbravo83@gmail.com>
2020-11-12 11:22:23 -0300

    auth flow ready

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const keys = require('./config/keys');
+const passport = require('passport');
+
+require('./models/User');
+
+require('./services/passport');
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology : true
+  })
+  .then(() => {
+    console.log("MongoDB connected!");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send("Hello There!");
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/myRoutes')(app);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

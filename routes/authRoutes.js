793710af92a933fc1537699a488aca15e09057f5
matias <matiasbravo83@gmail.com>
2020-11-15 15:26:05 -0300

    logo image url fixed?

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 module.exports = app => {
 	app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email']}));
 
-	app.get('/auth/google/callback', passport.authenticate('google'));
+	app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => { res.redirect('/')});
 
 	app.get('/api/logout', (req, res) => {
 		req.logout();

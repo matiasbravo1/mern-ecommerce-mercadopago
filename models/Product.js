d62e4775c23686459b7ecc7aad57adca8b7a3a6a
matias <matiasbravo83@gmail.com>
2020-11-25 11:17:47 -0300

    modif product model

--- models/Product.js
+++ models/Product.js
@@ -5,10 +5,17 @@ const productSchema = new Schema({
   name: String,
   brand: String,
   category: Number,
+  subcategory: Number,
   code: String,
-  description: String,
+  short_description: String,
+  large_description: String,
+  presentation: String,
+  in: Number,
+  out: Number,
+  reserved: Number,
   price: { type: Number, default: 0 },
   cost_price: { type: Number, default: 0 },
+  show: { type: Boolean, default: true },
   date_created: { type: Date, default: Date.now }
 });
 
